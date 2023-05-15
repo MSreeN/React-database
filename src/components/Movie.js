39643fde5ed6e4c0bd4a59b2d0552e9b47b16650
MSreeN <khyathimnsree369@gmail.com>
2023-05-15 17:30:06 +0530

    implemented delete hover feature

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useRef } from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
+
+  const delRef = useRef();
+  function deleteIconHandler(e){
+    const par = e.target.closest("li");
+    const delIcon = delRef.current;
+    delIcon.style.display = "block";
+    console.log("in");
+  }
+
+  function deleteIconOutHandler(e){
+    delRef.current.style.display = "none";
+    console.log("out");
+  }
+
   return (
-    <li className={classes.movie}>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-</svg>
+    <li className={`${classes.movie} movieEach`} onMouseEnter = {deleteIconHandler} onMouseLeave = {deleteIconOutHandler}>
+    <div ref={delRef} className={classes['del-container']}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+        className="svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+        />
+      </svg>
+    </div>
 
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,11 +4,13 @@ import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
+
+
   return (
 
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
-        <Movie
+        <Movie 
           key={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
